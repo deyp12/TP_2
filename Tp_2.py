0bf9a322c99a7f83dd147d9dd05f6bd6649e2ae0
deyp12 <deyp@educ.college-montreal.qc.ca>
2022-10-26 09:18:16 -0400

    commit n_1

--- Tp_2.py
+++ Tp_2.py
@@ -1,53 +1,52 @@
 """
 Tp 2
 Polashi Dey
-"""
+406
+description:Ce code est suppposé d'être un jeu de devinette,c'est à dire que joeur doit deviner le nombre choisi par hasard par le logiciel  """
 
 import random
 
+
+
 print("Bienvenue,Dans ce jeu,Il faut que vous devinez le nombre au aleratoire")
 
-"""
 
-"""
+
 def jeu_devinette():
 
-   borne_minimale = int(input("C'est quoi le nombre minimale voulez vouz avoir dans ce jeu "))
-   borne_maximale = int(input("C'est quoi le nombre maximale voulez vouz avoir dans ce jeu "))
-
-   x = random.randint (borne_minimale,borne_maximale)
-
-   nb_essai = 1
-   print("Allons y")
-   essai = int(input("entrez votre essai"))
+"""
+Ce code va donner la choix au joeur d'avoir le nombre minimal et nombre maximale du nombre qu'il doit deviner,ainsi cela va donner le nombre de essais que le joueur
+a prit de deviner et donner le choix de rejoueur ou de quitter à la fin du première partie du jeu
+"""
 
 
-   while essai != x:
+    borne_minimale = int(input("C'est quoi le nombre minimale voulez vouz avoir dans ce jeu "))
+    borne_maximale = int(input("C'est quoi le nombre maximale voulez vouz avoir dans ce jeu "))
 
+    x = random.randint(borne_minimale, borne_maximale)
 
+    nb_essai = 1
+    print("Allons y")
+    essai = int(input("entrez votre essai"))
 
-       if essai > x :
-           print ("le nombre est plus petit")
-           essai = int(input("entrez votre essai"))
-           nb_essai = nb_essai +1
-       else:
-           print("le nombre est plus grand")
-           essai = int(input("entrez votre essai"))
-           nb_essai = nb_essai+1
+    while essai != x:
 
+        if essai > x:
+            print("le nombre est plus petit")
+            essai = int(input("entrez votre essai"))
+            nb_essai = nb_essai + 1
+        else:
+            print("le nombre est plus grand")
+            essai = int(input("entrez votre essai"))
+            nb_essai = nb_essai + 1
 
-   print("Bonne réponse!")
-   print(f"vous avez dévinez en {nb_essai} essai")
+    print("Bonne réponse!")
+    print(f"vous avez dévinez en {nb_essai} essai")
 
-
-   quit = (input("Voulez vous quitter?Répondez par oui ou non "))
-   if quit == "oui":
-       print("Au revoir")
-   elif quit == "non":
-       jeu_devinette()
+    quit = (input("Voulez vous quitter?Répondez par oui ou non "))
+    if quit == "oui":
+        print("Au revoir")
+    elif quit == "non":
+        jeu_devinette()
 
 jeu_devinette()
-
-
-
-
